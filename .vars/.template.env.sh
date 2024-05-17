2b1f68fb522e14d449c0bbd412247975c6b666c0
Rashid Mohammed <rasheed.rmoh@gmail.com>
2024-05-17 13:58:30 +0000

    Merged PR 31: Add new env var
    Add new env var

--- .vars/.template.env.sh
+++ .vars/.template.env.sh
@@ -6,6 +6,7 @@ export TF_VAR_TENANT_ID=""
 export TF_VAR_CLIENT_ID=""
 export TF_VAR_CLIENT_SECRET=""
 export TF_VAR_SUBSCRIPTION_ID=""
+export TF_VAR_CONTAINERAPP_JOB_SCOPE=""
 
 # Snowflake Vars
 export snowflake_username=""
