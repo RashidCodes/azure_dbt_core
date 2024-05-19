baf8739bb07b3acc298da1e99837132f839cac80
Kingmoh <rashidmincan@gmail.com>
2024-05-19 22:14:22 +1000

    fix entra_application.tf

--- IaC-ServicePrincipal/variables.tf
+++ IaC-ServicePrincipal/variables.tf
@@ -2,3 +2,9 @@ variable "TENANT_ID" {
    description = "Tenant Id of the application running terraform"
    sensitive   = true
 }
+
+variable "CONTAINERAPP_JOB_SCOPE"{
+   description = "containerapp job resource id"
+   sensitive   = false
+}
+
